6b217c6e9f6acc0e84ed46da36efcfb0031e9fba
mkoertgen <marcel.koertgen@schoenhofer.de>
2015-04-13 14:10:34 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# hello.squirrel
+
+Getting to know [Squirrel.Windows](https://github.com/Squirrel/Squirrel.Windows) by going through the [Getting Started](https://github.com/Squirrel/Squirrel.Windows/blob/master/docs/getting-started.md) guide.
+
---
+++ hello.squirrel/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Squirrel;
+
+namespace hello.squirrel
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void UpdateOnClick(object sender, RoutedEventArgs e)
+        {
+            using (var mgr = new UpdateManager("https://path/to/my/update/folder", "hello.squirrel", FrameworkVersion.Net45))
+            {
+                await mgr.UpdateApp();
+            }
+        }
+    }
+}
